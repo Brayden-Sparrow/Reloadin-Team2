2a06e56f24f71b22eb8185749ea580a31f53f003
TheAuthenticRaccoon <92711886+OscarCastle@users.noreply.github.com>
2021-11-08 20:38:02 +1030

    Movement Update 8/11/2021

--- Reloadin-v2/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Reloadin-v2/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
     // Variables
 
     [Header("Magnitude")]
-    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float walkSpeed;
 
     [Header("Direction")]
     private Vector3 moveDirection;
@@ -19,14 +20,23 @@ public class PlayerMovement : MonoBehaviour
     [Header("Other")]
     [SerializeField] private bool isGrounded;
     [SerializeField] private float groundDistance = 0.1f;
-    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private LayerMask Ground;
     [SerializeField] private float gravity;
+    [SerializeField] private Vector3 targetPosition;
+    public GameObject BeholderEye;
 
     // References
 
     private CharacterController characterController;
 
     // Functions
+
+    private void Awake()
+    {
+        // Ignores everything except the ground
+        Ground = LayerMask.GetMask("Ground");
+
+    }
 
     private void Start()
     {
@@ -35,20 +45,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        Movement();
+        Move();
         Look();
     }
 
     // Script to move the player character depending on the input from the player.
-    private void Movement()
+    private void Move()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundDistance, groundMask);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundDistance, Ground);
 
         if (isGrounded == true && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
+        walkSpeed = movementSpeed * Time.deltaTime;
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
         moveDirection = new Vector3(moveX, 0, moveZ);
@@ -72,17 +83,20 @@ public class PlayerMovement : MonoBehaviour
     private void Look()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
 
         // Stores hit information
         RaycastHit hit;
 
         // Test to see if the ray hit the ground ignoring everything else.
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, Ground))
         {
-            var lookPos = hit.transform.position - transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 0.1f);
+            // Lock the y.axis and save the x and z position
+            targetPosition = new Vector3 (hit.point.x, transform.position.y, hit.point.z);
+
+            // Rotate the eye
+            BeholderEye.transform.LookAt(targetPosition);
         }
+
     }
 }
