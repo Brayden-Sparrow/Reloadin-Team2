756da14d0042d22982e291cb8ab11c83193b1583
GITDaer19 <87466852+GITDaer19@users.noreply.github.com>
2021-11-22 17:10:08 +1000

    petes

---
+++ Reloadin-v2/Assets/Scripts/Title Scripts/level_changer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+
+public class level_changer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void level1()
+    {
+        EditorSceneManager.LoadScene("oscar");
+    }
+}
