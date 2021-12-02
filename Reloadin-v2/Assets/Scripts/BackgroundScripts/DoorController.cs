8ac46c846026fd56036e4dda1ba9eb45715df081
TheAuthenticRaccoon <92711886+OscarCastle@users.noreply.github.com>
2021-12-02 14:53:16 +1030

    Big Room & Door Controller Update 2

--- Reloadin-v2/Assets/Scripts/BackgroundScripts/DoorController.cs
+++ Reloadin-v2/Assets/Scripts/BackgroundScripts/DoorController.cs
@@ -6,14 +6,18 @@ public class DoorController : MonoBehaviour
 {
     // Variables
 
-    public float dampTime = 0.2f;
-
     // References
 
     public GameObject door1;
     public GameObject door2;
+    public GameObject door3;
+    public GameObject door4;
+    public GameObject door5;
     public GameObject key1;
     public GameObject key2;
+    public GameObject key3;
+    public GameObject key4;
+    public GameObject key5;
 
     // Functions
 
@@ -21,8 +25,13 @@ public class DoorController : MonoBehaviour
     {
         key1.SetActive(true);
         key2.SetActive(true);
+        key3.SetActive(true);
+        key4.SetActive(true);
         door1.SetActive(true);
         door2.SetActive(true);
+        door3.SetActive(true);
+        door4.SetActive(true);
+        door5.SetActive(true);
     }
 
     // Update is called once per frame
@@ -41,6 +50,18 @@ public class DoorController : MonoBehaviour
         if (key2.activeInHierarchy == false)
         {
             door2.SetActive(false);
+        }
+        if (key3.activeInHierarchy == false)
+        {
+            door3.SetActive(false);
+        }
+        if (key4.activeInHierarchy == false)
+        {
+            door4.SetActive(false);
+        }
+        if (key5.activeInHierarchy == false)
+        {
+            door5.SetActive(false);
         }
     }
 }
