196ca890292e57fd4d480fd3a7bc9fbbfe89a399
GITDaer19 <87466852+GITDaer19@users.noreply.github.com>
2021-12-15 17:26:24 +1000

    Wing Update

---
+++ Reloadin-v2/Assets/Scripts/Player Scripts/WingFlap.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WingFlap : MonoBehaviour
+{
+    private float starting_y_rotation;
+    public float magnitude;
+    public float period;
+
+    private GameObject mother;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
